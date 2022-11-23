bca40876d5dcab69c5190d235dd05f9e651ac2e2
Oliver Berg <oliver@berg.moe>
2022-11-23 20:03:36 +0100

    Initial commit

---
+++ src/main/kotlin/net/ormr/usersrkipter/example/main.kt
@@ -0,0 +1,23 @@
+package net.ormr.usersrkipter.example
+
+import kotlinx.browser.document
+import kotlinx.coroutines.launch
+import net.ormr.userskripter.engine.greasemonkey.GreaseMonkey
+import net.ormr.userskripter.engine.greasemonkey.notification
+import net.ormr.userskripter.userskript
+import net.ormr.userskripter.util.appendNewElement
+import org.w3c.dom.HTMLButtonElement
+
+fun main() = userskript {
+    val element = document.querySelector("body > div")!!
+    element.appendNewElement<HTMLButtonElement>("button") {
+        textContent = "Click Me!"
+        onclick = {
+            launch {
+                GreaseMonkey.notification {
+                    text = "This is a notification!"
+                }
+            }
+        }
+    }
+}
